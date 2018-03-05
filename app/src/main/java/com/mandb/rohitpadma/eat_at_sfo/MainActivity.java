99d445275a242d546976d454a0f60f0c9488473f
Rohit Padma <rohitp32@gmail.com>
2018-03-05 14:33:49 -0500

    Initial project set up

---
+++ app/src/main/java/com/mandb/rohitpadma/eat_at_sfo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mandb.rohitpadma.eat_at_sfo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
