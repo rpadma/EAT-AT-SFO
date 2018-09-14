d0e1977f974941f6769f33f7e1ff05663227fd12
Rohit Padma <rohitp32@gmail.com>
2018-09-14 00:01:37 -0400

    Added sample lottie animation in splash screen

--- app/src/main/java/com/mandb/rohitpadma/eat_at_sfo/SplashActivity.java
+++ app/src/main/java/com/mandb/rohitpadma/eat_at_sfo/SplashActivity.java
@@ -1,12 +1,18 @@
 package com.mandb.rohitpadma.eat_at_sfo;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.airbnb.lottie.LottieAnimationView;
+import com.airbnb.lottie.LottieComposition;
+
 public class SplashActivity extends AppCompatActivity {
+
+    LottieAnimationView mLottieAnimationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,12 +20,20 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+        mLottieAnimationView = findViewById(R.id.lottie_animation_view);
+
+        LottieComposition.Factory.fromAssetFileName(this, "lottie.json", composition -> {
+            mLottieAnimationView.setComposition(composition);
+            mLottieAnimationView.playAnimation();  });
+
         Thread runnerlog=new Thread()
         {
             public void run()
             {
                 try
                 {
+
+
                     int logoTimer=0;
                     while(logoTimer<2000)
                     {
@@ -42,6 +56,14 @@ public class SplashActivity extends AppCompatActivity {
         };
 
         runnerlog.start();
+
+       // bindAnim();
+    }
+
+    public void bindAnim(){
+        new Handler().post(() -> {
+
+        });
     }
 
 
@@ -50,6 +72,8 @@ public class SplashActivity extends AppCompatActivity {
         // TODO Auto-generated method stub
         super.onDestroy();
         unbindDrawables(findViewById(R.id.splashlayout));
+        mLottieAnimationView.cancelAnimation();
+        mLottieAnimationView = null;
         System.gc();
     }
 
